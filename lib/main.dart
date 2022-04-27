174c5357c7b1dee16b5dba3250b1f01e9a4b3bbb
Doğukan 'Esen' Balaman <dodikbalaman@gmail.com>
2022-04-27 11:50:54 +0300

    Self Input Mesajlar

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,45 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: avoid_print
+import 'dart:math';
 
-void main() {
-  runApp(const MyApp());
-}
+void main(List<String> args) {
+  const mesajlar = ['a', 'b', 'c'];
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  print(mesajlar); // ---> [a,b,c]
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.teal,
-        title: const Text('Flutter'),
-      ),
-    ));
+  print(mesajlar[0]); // ---> a
+
+  mesajlar.add('d');
+  print(mesajlar); // ---> [a,b,c,d]
+
+  mesajlar.addAll(['e', 'f']);
+  print(mesajlar); // ---> [a,b,c,d,e,f]
+
+  Random random = Random();
+  int randomSayi = random.nextInt(6);
+
+  mesajlar.removeRange(randomSayi, 6);
+
+  print('${mesajlar.length} adet mesajınız var.)');
+
+  mesajlar.shuffle();
+  print(mesajlar);
+
+  for (final mesaj in mesajlar) {
+    print(mesaj);
   }
+
+  if (mesajlar.contains('a')) {
+    print('a var');
+  } else {
+    print('a yok');
+  }
+
+  mesajlar[0] = 'A';
+  print("Artık ilk mesajınız 'A' ve mesaj sayınız ${mesajlar.length}'dir.");
+
+  print("İlk mesajınız ${mesajlar.first}'dır.");
+
+  print("Son mesajınız ise ${mesajlar.last}'dir.");
+
+  print('Mesajlarınız tam listesi bunlardır $mesajlar');
 }
